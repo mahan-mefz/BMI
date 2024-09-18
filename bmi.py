0a559cedfe9dca49425d20ed2ea876521dba1b35
mahan <mahan.mehrafza@gmail.com>
2024-09-18 17:54:35 +0330

    فایل در دست دلولپر قرار گرفت

--- bmi.py
+++ bmi.py
@@ -1,16 +1,26 @@
-import time
-
 class mbi :
-    #def __init__(self):
-    def mbi (self) :
-        vazn = int(input("vazn ra vared konid : "))
-        gad = int(input("gad ra vared konid : "))
-        
-        
+    def mbi1 (self) :
+        self.vazn = int(input("vazn ra vared konid : "))
+        self.gad = int(input("gad ra vared konid : "))
+        self.mbimetr()
 
-    
+    def mbimetr(self) :
+        self.metr = self.gad /100
+        print(self.metr)
+        
+                
+class mbismart(mbi) :
+    def smart (self):
+        self.vaznee = mbi.vazn
+        self.gadd = mbi.metr
+        print(self.gadd)
+        self.gadd = self.gadd ** 2
+        print(self.gadd)
+        self.enf = self.vaznee / self.gadd
+        print(self.enf)
 
 mbi = mbi()
-
+mbismart = mbismart()
 while True :
-    mbi.mbi()
+    mbi.mbi1()
+    mbismart.smart()
