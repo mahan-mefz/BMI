de065428bc4c4ec21fa23cdf408fe367e1856369
mahan <mahan.mehrafza@gmail.com>
2024-09-18 15:27:59 +0330

    قسمت دریافت اطلاعات از کاربر کامل شد

---
+++ bmi.py
@@ -0,0 +1,16 @@
+import time
+
+class mbi :
+    #def __init__(self):
+    def mbi (self) :
+        vazn = int(input("vazn ra vared konid : "))
+        gad = int(input("gad ra vared konid : "))
+        
+        
+
+    
+
+mbi = mbi()
+
+while True :
+    mbi.mbi()
